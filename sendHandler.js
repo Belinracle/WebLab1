a0496cbc6b4ff8874a31e0e1352c5b988170915b
Daniel <rosdanas@gmail.com>
2020-08-10 14:07:51 +0300

    пока работает

--- sendHandler.js
+++ sendHandler.js
@@ -1,4 +1,7 @@
 'use strict';
+let Rvalues = document.querySelectorAll('[name=R]');
+let Xvalues =  document.querySelectorAll('[name=X]');
+let point = document.querySelector('#point')
 document.querySelector('#sendButton').addEventListener('click',function(){
     let color = window.getComputedStyle(document.querySelector('#form')).backgroundColor;
     if (color === 'rgb(0, 153, 0)')
@@ -13,13 +16,18 @@ document.querySelector('#sendButton').addEventListener('click',function(){
         request.addEventListener("readystatechange", () =>  {
             if (request.readyState === 4 && request.status === 200) {
                 document.querySelector('#results').innerHTML+=request.responseText;
+                if(request.responseText.includes('not hited')){
+                    resetPoint();
+                }
+                else {
+                    setPoint(X, Y, R);
+                }
             }
         });
         request.send(data);
     }
 });
 function getXvalue(){
-    let Xvalues =  document.querySelectorAll('[name=X]');
     for (let i = 0;i<Xvalues.length;i++)
         if (Xvalues[i].checked===true)
             return Xvalues[i].getAttribute('value');
@@ -32,7 +40,6 @@ function filterFloat(value) {
   return 'NaN';
 }
 function getRvalue(){
-    let Rvalues = document.querySelectorAll('[name=R]');
     for (let i = 0 ; i<Rvalues.length; i++){
         if (Rvalues[i].classList.contains('activated'))
             return Rvalues[i].getAttribute('value');
@@ -40,4 +47,14 @@ function getRvalue(){
 }
 document.querySelector('#clearButton').addEventListener('click',function() {
     document.querySelector('#results').innerHTML = '<tr><th>X</th><th>Y</th><th>R</th><th>Current Time</th><th>Execution Time</th><th>Result</th></tr>';
+    point.innerHTML='';
 });
+function setPoint(x,y,r){
+    let cx = 250 + x/r*200;
+    let cy = 250 - y/r*200;
+    resetPoint();
+    point.innerHTML='<circle class="new" r="2" cx="'+cx+'" cy="'+cy+'" />'
+}
+function resetPoint(){
+    point.innerHTML='';
+}
--- validator.js
+++ validator.js
@@ -1,5 +1,5 @@
 let inputs = document.querySelectorAll("[name = X]");
-let buttons = document.querySelectorAll('[name=R]').for;
+let buttons = document.querySelectorAll('[name=R]');
 let textField = document.querySelector('[name=Y]');
 let form = document.querySelector('#form');
 document.querySelector('[name=Y]').addEventListener('input',isValid);
@@ -12,54 +12,55 @@ function checkboxHandler(e) {
         if (inputs[i].checked && inputs[i] !== this)
             inputs[i].checked = false;
 }
-        
+
 for (let i = 0 ; i<buttons.length; i++){
     buttons[i].addEventListener('click',function(){
         if ( this.classList.contains('activated'))
             this.classList.remove("activated");
         else
             this.classList.add("activated");
-    for (let b =0;b<buttons.length; b++){
-        if(buttons[b].classList.contains('activated') && buttons[b] !== this){
-            buttons[b].classList.remove("activated");
+        for (let b =0;b<buttons.length; b++){
+            if(buttons[b].classList.contains('activated') && buttons[b] !== this){
+                buttons[b].classList.remove("activated");
+            }
         }
-    }
-});
+    });
     buttons[i].addEventListener('click', isValid);
 }
 
 function isValid(){
     if(checkboxes()&&butts()&&text())
         form.style.background = 'rgb(0,153,0)';
-    else 
+    else
         form.style.background = 'rgb(197,29,52)';
 }
 function checkboxes(){
     let okay = false;
     for (let i = 0; i < inputs.length; i++)
-        if (inputs[i].checked) 
+        if (inputs[i].checked)
             okay = true;
     return okay;
 }
 function butts(){
     let okay = false;
     for (let i = 0; i < buttons.length; i++)
-        if (buttons[i].classList.contains('activated')) 
+        if (buttons[i].classList.contains('activated'))
             okay = true;
     return okay;
 }
 function text(){
     let num = filterFloat(textField.value.replace(",",".").trim());
+
     if (num!=='NaN'){
         if (num>=-5&&num<=3)return true;
         else return false;
     }
     else return false;
 }
-let filterFloat = function (value) {
+var filterFloat = function (value) {
     if (value.length>14) return 'NaN';
     if(/^(-|\+)?([0-9]+(\.[0-9]+)?)$/
-      .test(value))
-      return Number(value);
-  return 'NaN';
+        .test(value))
+        return Number(value);
+    return 'NaN';
 };
